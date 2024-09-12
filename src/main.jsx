0b6b9f75ef211ec7c99c71f1223ce162a46f719d
Tan Khang <tankhang101a@gmail.com>
2024-09-12 10:27:00 +0700

    add conform password

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import {store} from './redux/store'
 
+
+
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
       <Provider store={store}>
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Button, Checkbox, Divider, Form, Input, message, notification } from 'antd';
-import { useNavigate } from "react-router-dom";
-import { callRegister } from "../../services/api.js";
+import {Button, Checkbox, Divider, Form, Input, message, notification} from 'antd';
+import {useNavigate} from "react-router-dom";
+import {callRegister} from "../../services/api.js";
 import "./RegisterForm.scss"
 
 const RegisterForm = () => {
@@ -10,10 +10,10 @@ const RegisterForm = () => {
 
     const onFinish = async (values) => {
         console.log('Success:', values);
-        const { name, email, password } = values;
+        const {name, email, password, confirmPassword} = values;
         setIsSubmit(true);
 
-        const res = await callRegister({ name, email, password });
+        const res = await callRegister({name, email, password, confirmPassword});
         setIsSubmit(false);
 
         if (res?.data) {
@@ -33,56 +33,67 @@ const RegisterForm = () => {
     };
 
     return (
-        <div className="register-page">
-            <h3 className="register-title">Register Form</h3>
-            <Divider />
-            <Form
-                name="basic"
-                labelCol={{ span: 24 }}
-                wrapperCol={{ span: 24 }}
-                className="register-form"
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <Form.Item
-                    labelCol={{ span: 24 }}
-                    label="Fullname"
-                    name="name"
-                    rules={[{ required: true, message: 'Please input your fullname!' }]}
+        <div className="register-container">
+            <div className="register-page">
+                <h3 className="register-title">Register</h3>
+                <Divider/>
+                <Form
+                    name="basic"
+                    labelCol={{span: 24}}
+                    wrapperCol={{span: 24}}
+                    className="register-form"
+                    initialValues={{remember: true}}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
                 >
-                    <Input />
-                </Form.Item>
+                    <Form.Item
+                        labelCol={{span: 24}}
+                        label="Fullname"
+                        name="name"
+                        rules={[{required: true, message: 'Please input your fullname!'}]}
+                    >
+                        <Input/>
+                    </Form.Item>
 
-                <Form.Item
-                    labelCol={{ span: 24 }}
-                    label="Email"
-                    name="email"
-                    rules={[{ required: true, type: 'email', message: 'Please input your email!' }]}
-                >
-                    <Input />
-                </Form.Item>
+                    <Form.Item
+                        labelCol={{span: 24}}
+                        label="Email"
+                        name="email"
+                        rules={[{required: true, type: 'email', message: 'Please input your email!'}]}
+                    >
+                        <Input/>
+                    </Form.Item>
 
-                <Form.Item
-                    labelCol={{ span: 24 }}
-                    label="Password"
-                    name="password"
-                    rules={[{ required: true, message: 'Please input your password!' }]}
-                >
-                    <Input.Password />
-                </Form.Item>
+                    <Form.Item
+                        labelCol={{span: 24}}
+                        label="Password"
+                        name="password"
+                        rules={[{required: true, message: 'Please input your password!'}]}
+                    >
+                        <Input.Password/>
+                    </Form.Item>
+                    <Form.Item
+                        labelCol={{span: 24}}
+                        label="Confirm password"
+                        name="confirmPassword"
+                        rules={[{required: true, message: 'Please input confirm password!'}]}
+                    >
+                        <Input.Password/>
+                    </Form.Item>
 
-                <Form.Item
-                    wrapperCol={{ span: 24 }}
-                >
-                    <Button type="primary" htmlType="submit" loading={isSubmit}>
-                        Register
-                    </Button>
-                </Form.Item>
-            </Form>
-            <div className="login-link">
-                <a onClick={() => navigate('/login')}> Login</a>
+                    <Form.Item
+                        wrapperCol={{span: 24}}
+                    >
+                        <Button type="primary" htmlType="submit" loading={isSubmit}>
+                            Register
+                        </Button>
+                    </Form.Item>
+                </Form>
+                <div className="login-link">
+                    <p>Already have a account? <a onClick={() => navigate('/login')}> Login</a></p>
+
+                </div>
             </div>
         </div>
     );
