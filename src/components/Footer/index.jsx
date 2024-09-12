1f776bc9b3c9ba2d1c38a65c61c318770ca57340
Tan Khang <tankhang101a@gmail.com>
2024-09-12 12:11:48 +0700

    nav and footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,28 @@
-import './footer.scss';
+import './index.css';
+import {BackTop} from "antd";
 
 const Footer = () => {
     return (
-        <footer>
-            <div>This is footer</div>
-        </footer>
-    )
+        <div className="container-fluid">
+            <div className="footer">
+                <div className="logo">
+                    <i className="fas fa-bolt"></i>
+                    <a href="http://google.com">好不好</a>
+                </div>
+                <ul className="socials">
+                    <li><a href="https://www.facebook.com/"><i className="fab fa-facebook-f"></i></a></li>
+                    <li><a href="https://www.twitter.com/"><i className="fab fa-twitter"></i></a></li>
+                    <li><a href="https://www.linkedin.com/"><i className="fab fa-linkedin-in"></i></a></li>
+                    <li><a href="https://www.pinterest.com/"><i className="fab fa-pinterest-p"></i></a></li>
+                    <li><a href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a></li>
+                </ul>
+                <div className="copyright">Copyright &copy; 2020 好不好</div>
+                <BackTop>
+                    <div className="goTop"><i className="fas fa-arrow-circle-up"></i></div>
+                </BackTop>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
--- src/components/Header/index..jsx
+++ src/components/Header/index..jsx
@@ -1,46 +1,47 @@
-import React, { useState } from 'react';
-import { FaReact } from 'react-icons/fa'
-import { FiShoppingCart } from 'react-icons/fi';
-import { VscSearchFuzzy } from 'react-icons/vsc';
-import { Divider, Badge, Drawer, message} from 'antd';
-import './header.scss';
+import React, {useState} from 'react';
+import {FaReact} from 'react-icons/fa'
+import {VscSearchFuzzy} from 'react-icons/vsc';
+import {Divider, Badge, Drawer, message, Button, Anchor} from 'antd';
+// import './header.scss';
 import {useDispatch, useSelector} from 'react-redux';
-import { DownOutlined } from '@ant-design/icons';
-import { Dropdown, Space } from 'antd';
-import {useNavigate} from "react-router-dom";
+import {DownOutlined} from '@ant-design/icons';
+import {Dropdown, Space} from 'antd';
+import {Link, useNavigate} from "react-router-dom";
 import {callLogout} from "../../services/api.js";
 import {doLogoutAction} from "../../redux/account/accountSlice.js";
+import './index.css'
+import {FiShoppingCart} from "react-icons/fi";
 
 
 const Header = () => {
     const [openDrawer, setOpenDrawer] = useState(false);
     const isAuthenticated = useSelector(state => state.account.isAuthenticated);
     const role = useSelector(state => state.account.user.role.name);
-    console.log("check menu",role);
+    console.log("check menu", role);
     const user = useSelector(state => state.account.user);
     console.log(user);
     const navigate = useNavigate();
 
     const dispatch = useDispatch();
 
-    const handleLogout = async () =>{
+    const handleLogout = async () => {
         const res = await callLogout();
-        console.log('logout res: ' , res);
-        if(res && res.statusCode === 200) {
+        console.log('logout res: ', res);
+        if (res && res.statusCode === 200) {
             dispatch(doLogoutAction());
             message.success('Đăng xuất thành công');
-            navigate('/');
+            navigate('/login');
         }
     }
 
     const items = [
         {
-            label: <label style={{ cursor: 'pointer' }}>Quản lý tài khoản</label>,
+            label: <label style={{cursor: 'pointer'}}>Quản lý tài khoản</label>,
             key: 'account',
         },
         ...(role === 'ROLE_ADMIN' ? [{
             label: <label
-                style={{ cursor: 'pointer' }}
+                style={{cursor: 'pointer'}}
                 onClick={() => navigate('/admin')}
             >
                 Trang quản lý Admin
@@ -49,75 +50,121 @@ const Header = () => {
         }] : []),
         {
             label: <label
-                style={{ cursor: 'pointer' }}
+                style={{cursor: 'pointer'}}
                 onClick={() => handleLogout()}
             >Đăng xuất</label>,
             key: 'logout',
         },
     ];
 
+    const [visible, setVisible] = useState(false);
 
+    const showDrawer = () => {
+        setVisible(true);
+    };
+
+    const onClose = () => {
+        setVisible(false);
+    };
     return (
         <>
-            <div className='header-container'>
-                <header className="page-header">
-                    <div className="page-header__top">
-                        <div className="page-header__toggle" onClick={() => {
-                            setOpenDrawer(true)
-                        }}>☰</div>
-                        <div className='page-header__logo'>
-                            <span className='logo'>
-                                <FaReact className='rotate icon-react' /> WEB NAME ?
-                                <VscSearchFuzzy className='icon-search' />
-                            </span>
-                            <input
-                                className="input-search" type={'text'}
-                                placeholder="Bạn tìm gì hôm nay"
-                            />
-                        </div>
-
+            <div className="container-fluid">
+                <div className="header">
+                    <div className="logo">
+                        <i className="fas fa-bolt"></i>
+                        <a href="http://google.com">好不好</a>
                     </div>
-                    <nav className="page-header__bottom">
-                        <ul id="navigation" className="navigation">
-                            <li className="navigation__item">
-                                <Badge
-                                    count={5}
-                                    size={"small"}
-                                >
-                                    <FiShoppingCart className='icon-cart' />
-                                </Badge>
-                            </li>
-                            <li className="navigation__item mobile"><Divider type='vertical' /></li>
-                            <li className="navigation__item mobile">
+                    <div className="mobileHidden">
+                        <nav>
+                            <div>
+                                <span onClick={() => navigate('/')}> Home</span>
+                            </div>
+                            <div>
+                                <span onClick={() => navigate('/1')}> Product</span>
+                            </div>
+                            <div>
+                                <span onClick={() => navigate('/2')}> Cart</span>
+                            </div>
+                            <div>
+                                <span onClick={() => navigate('/3')}> About</span>
+                            </div>
+                            {/*<div>*/}
+                            {/*<Badge*/}
+                            {/*        count={0}*/}
+                            {/*        size={"small"}*/}
+                            {/*    >*/}
+                            {/*        <FiShoppingCart className='icon-cart'/>*/}
+                            {/*    </Badge>*/}
+                            {/*</div>*/}
+                            <div>
                                 {!isAuthenticated || user === null ?
-                                    <span onClick={() => navigate('/login')}> Tài Khoản</span>
+                                    <span onClick={() => navigate('/login')}> Account</span>
                                     :
-                                    <Dropdown menu={{ items }} trigger={['click']}>
+                                    <Dropdown menu={{items}} trigger={['click']}>
                                         <a onClick={(e) => e.preventDefault()}>
                                             <Space>
-                                                Welcome {user?.name}
-                                                <DownOutlined />
+                                                <span style={{ fontSize: '16px'}}>
+                                                    Welcome: <span style={{textTransform: 'uppercase'}}> {user?.name} </span>
+                                                </span>
+                                                <DownOutlined/>
                                             </Space>
                                         </a>
                                     </Dropdown>
                                 }
-                            </li>
-                        </ul>
-                    </nav>
-                </header>
-            </div>
-            <Drawer
-                title="Menu chức năng"
-                placement="left"
-                onClose={() => setOpenDrawer(false)}
-                open={openDrawer}
-            >
-                <p>Quản lý tài khoản</p>
-                <Divider />
+                            </div>
+                        </nav>
+                    </div>
+                    <div className="mobileVisible">
+                        <Button type="primary" onClick={showDrawer}>
+                            <i className="fas fa-bars"></i>
+                        </Button>
+                        <Drawer
+                            placement="right"
+                            closable={false}
+                            onClose={onClose}
+                            visible={visible}
+                        >
+                            <nav>
+                                <div>
+                                    <span onClick={() => navigate('/')}> Home</span>
+                                </div>
+                                <div>
+                                    <span onClick={() => navigate('/1')}> Product</span>
+                                </div>
+                                <div>
+                                    <span onClick={() => navigate('/2')}> Cart</span>
+                                </div>
+                                <div>
+                                    <span onClick={() => navigate('/3')}> About</span>
+                                </div>
+                                {/*<div>*/}
+                                {/*<Badge*/}
+                                {/*        count={0}*/}
+                                {/*        size={"small"}*/}
+                                {/*    >*/}
+                                {/*        <FiShoppingCart className='icon-cart'/>*/}
+                                {/*    </Badge>*/}
+                                {/*</div>*/}
+                                <div>
+                                    {!isAuthenticated || user === null ?
+                                        <span onClick={() => navigate('/login')}> Account</span>
+                                        :
+                                        <Dropdown menu={{items}} trigger={['click']}>
+                                            <a onClick={(e) => e.preventDefault()}>
+                                                <Space>
+                                                    Welcome {user?.name}
+                                                    <DownOutlined/>
+                                                </Space>
+                                            </a>
+                                        </Dropdown>
+                                    }
+                                </div>
+                            </nav>
+                        </Drawer>
+                    </div>
 
-                <p>Đăng xuất</p>
-                <Divider />
-            </Drawer>
+                </div>
+            </div>
         </>
     )
 };
